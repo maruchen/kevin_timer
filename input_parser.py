124dd1ceb5fcf323a74383ec77105a20eedc2ff0
maruchen <chenmaru@gmail.com>
2014-10-17 09:54:38 +0800

    完成新版input parser 更新readme

--- input_parser.py
+++ input_parser.py
@@ -9,8 +9,6 @@ import argparse
 import operator
 import math
 
-#TODO 单测
-
 
 DEFAULT_TIME_OF_DAY = "8:00"
 
@@ -20,102 +18,6 @@ class FormatError(Exception):
 
 class InputParser(object):
     # timestamp is in seconds
-    def _get_timestamp_from_now(self, input_time):
-        match = re.match(r'(\d+)(\D+)', input_time)
-        if match is not None:
-            multiple = 60
-            if match.group(2) == 'm':
-                multiple = 60
-            elif match.group(2) == 'd':
-                multiple = 60 * 24
-            absolute_timestamp = time.mktime(time.localtime()) + int(match.group(1)) * multiple
-            return absolute_timestamp
-
-        raise FormatError("value error")
-
-
-    def _get_timestamp_from_static_day(self, input_time):
-        standard_time_string = "";
-        match = re.match(r'\d+-\d+-\d+', input_time)
-        if match is not None:
-            standard_time_string = input_time + " " + DEFAULT_TIME_OF_DAY
-
-        else:
-            match = re.match(r'\d+-\d+', input_time)  # no year
-            if match is not None:
-                year = time.localtime().tm_year
-                standard_time_string = str(year) + "-" + input_time + " " + DEFAULT_TIME_OF_DAY
-
-            else:
-                raise FormatError("value error")
-
-        absolute_timestamp = time.mktime(time.strptime(standard_time_string, "%Y-%m-%d %H:%M:%S")) 
-        return absolute_timestamp
-
-
-    def _get_timestamp_from_static_time(self, input_time):
-        standard_time_string = time.strftime("%Y-%m-%d", time.localtime())       
-        match = re.match(r'\d+:\d+:\d+', input_time)
-        if match is not None:
-            standard_time_string = standard_time_string + " " + input_time
-
-        else:
-            match = re.match(r'\d+:\d+', input_time)  # no seconds
-            if match is not None:
-                standard_time_string = standard_time_string + " " + input_time + ":00"
-
-            else:
-                raise FormatError("value error")
-
-        absolute_timestamp = time.mktime(time.strptime(standard_time_string, "%Y-%m-%d %H:%M:%S")) 
-        return absolute_timestamp
-
-
-    def _convert_1param_to_timestamp(self, input_time):
-        match = re.match(r'\d+[md]', input_time)
-        if match is not None:
-            absolute_timestamp = self._get_timestamp_from_now(input_time)
-            return absolute_timestamp
-        
-        match = re.match(r'(\d+-)?\d+-\d+', input_time)
-        if match is not None:
-            absolute_timestamp = self._get_timestamp_from_static_day(input_time)
-            return absolute_timestamp
-
-        match = re.match(r'\d+:\d+(:\d+)?', input_time)
-        if match is not None:
-            absolute_timestamp = self._get_timestamp_from_static_time(input_time)
-            return absolute_timestamp
-
-        raise FormatError("value error")
-
-
-    def _convert_2param_to_timestamp(self, input_day, input_time):
-        standard_time_string = "";
-        match = re.match(r'\d+-\d+-\d+', input_day)
-        if match is not None:
-            standard_time_string = input_day
-        else:
-            match = re.match(r'\d+-\d+', input_day)  # no year
-            if match is not None:
-                year = time.localtime().tm_year
-                standard_time_string = str(year) + "-" + input_day
-            else:
-                raise FormatError("input_day error")
-
-        match = re.match(r'\d+:\d+:\d+', input_time)
-        if match is not None:
-            standard_time_string = standard_time_string + " " + input_time
-        else:
-            match = re.match(r'\d+:\d+', input_time)  # no seconds
-            if match is not None:
-                standard_time_string = standard_time_string + " " + input_time + ":00"
-            else:
-                raise FormatError("input_time error")
-
-        absolute_timestamp = time.mktime(time.strptime(standard_time_string, "%Y-%m-%d %H:%M:%S")) 
-        return absolute_timestamp
-
 
     def parse_arguments(self):
         """
@@ -124,7 +26,7 @@ class InputParser(object):
         if len(sys.argv) <= 1:
             raise FormatError("参数太少") 
         # 第一个参数为脚本名，排除
-        cmdline = ' '.join(argv[1:]) 
+        cmdline = ' '.join(sys.argv[1:]) 
         return self.parse_textline(cmdline)
 
 
--- timer.py
+++ timer.py
@@ -25,22 +25,27 @@ logging.basicConfig(filename='D:/kevin_timer.log',
 if __name__ == "__main__":
     logging.debug("input:%s", ' '.join(sys.argv))
 
-    #print len(sys.argv)
-    #print ' '.join(sys.argv)
-    #print os.path.abspath('.') 
-    #time.sleep(1000)
     try:
         parser = InputParser()
-        (absolute_timestamp, input_todo) = parser.parse_arguments(sys.argv)
+        (absolute_timestamp, input_todo) = parser.parse_arguments()
 
     except FormatError as e:
         logging.error("arguments must be in format: time something")
         print "arguments must be in format: time something"
         print "TIPS: examples of time: 5m(5 minutes later), 3d(3 days later), or 11-01(the Nov. 1st in this year), or 2015-1-2, or [2015-]1-2 8:00[:00]"
+        time.sleep(10)
         exit(1)
 
+    except Exception as e:
+        logging.error("unknown exception:%s", str(e))
+        print str(e)
+        time.sleep(10)
+        exit(1)
 
+    print "done"
     print input_todo + "\t@ " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(absolute_timestamp)) + " (" + str(absolute_timestamp) + ")"
+    logging.debug("parse ok:%s", input_todo + "\t@ " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(absolute_timestamp)) + " (" + str(absolute_timestamp) + ")")
+    time.sleep(1)
     with open(WATCH_FILENAME, 'a') as watch_file:
         watch_file.write(input_todo + "\t@ " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(absolute_timestamp)) + " (" + str(absolute_timestamp) + ")\n")
 
